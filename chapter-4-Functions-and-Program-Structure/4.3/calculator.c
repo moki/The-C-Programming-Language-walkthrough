ded3041bbdb5c0f82a25db110f289b58ac94f4f0
Kirill Morozov (moki) <morozov.kirill.moki@gmail.com>
2017-06-21 19:11:00 +0300

    Add 4.3 External Variables

---
+++ chapter-4-Functions-and-Program-Structure/4.3/calculator.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAXOP 100
+#define NUMBER '0'
+
+#define MAXVAL 100
+
+#define BUFSIZE 100
+
+int getop(char []);
+int getch(void);
+void ungetch(int);
+void push(double);
+double pop(void);
+
+int main(void) {
+  int type;
+
+  double op2;
+
+  char str[MAXOP];
+
+  while ((type = getop(str)) != EOF) {
+    switch (type) {
+      case NUMBER:
+        push(atof(str));
+        break;
+      case '+':
+        push(pop() + pop());
+        break;
+      case '*':
+        push(pop() * pop());
+        break;
+      case '-':
+        op2 = pop();
+        push(pop() - op2);
+        break;
+      case '/':
+        op2 = pop();
+        if (op2 != 0.0) {
+          push(pop() / op2);
+        } else {
+          printf("error: zero divisor \n");
+        }
+        break;
+      case '\n':
+        printf("\t%.8g\n", pop());
+        break;
+      default:
+        printf("error: unknown command %s\n", str);
+        break;
+    }
+  }
+
+  return 0;
+}
+
+int sp = 0;
+double v[MAXVAL];
+
+void push(double f) {
+  if (sp < MAXVAL) {
+    v[sp++] = f;
+  } else {
+    printf("error: stack full, can't push %g\n", f);
+  }
+}
+
+double pop(void) {
+  if (sp > 0) {
+    return v[--sp];
+  } else {
+    printf("error: stack empty\n");
+    return 0.0;
+  }
+}
+
+int getop(char s[]) {
+  int i, c;
+  
+  while ((s[0] = c = getch()) == ' ' || c == '\t');
+
+  s[1] = '\0';
+
+  if (!isdigit(c) && c != '.') {
+    return c;
+  }
+
+  i = 0;
+
+  if (isdigit(c)) {
+    while (isdigit(s[++i] = c = getch()));
+  }
+
+  if (c == '.') {
+    while (isdigit(s[++i] = c = getch()));
+  }
+
+  s[i] = '\0';
+
+  if (c != EOF) {
+    ungetch(c);
+  }
+  
+  return NUMBER;
+}
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void) {
+  return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+  if (bufp >= BUFSIZE) {
+    printf("ungetch: too many characters\n");
+  } else {
+    buf[bufp++] = c;
+  }
+}
